3cb37e1f1c006003417effb0064d3e3c5752111c
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-06-15 19:37:39 +0600

    Update EmployeeController files

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EmployeeResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EmployeeController extends Controller
 {
@@ -14,7 +17,10 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+        $employees = Employee::all();
+        return response([ 'employees' => 
+        EmployeeResource::collection($employees), 
+        'message' => 'Successful'], 200);
     }
 
     /**
@@ -25,40 +31,67 @@ class EmployeeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required|max:50',
+            'age' => 'required|max:50',
+            'job' => 'required|max:50',
+            'salary' => 'required|50'
+        ]);
+
+        if($validator->fails()){
+            return response(['error' => $validator->errors(), 
+            'Validation Error']);
+        }
+
+        $employee = Employee::create($data);
+
+        return response([ 'employee' => new 
+        EmployeeResource($employee), 
+        'message' => 'Success'], 200);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Employee  $employee
+     * @param  \App\Employee  $employee
      * @return \Illuminate\Http\Response
      */
     public function show(Employee $employee)
     {
-        //
+        return response([ 'employee' => new 
+        EmployeeResource($employee), 'message' => 'Success'], 200);
+
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Employee  $employee
+     * @param  \App\Employee  $employee
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Employee $employee)
     {
-        //
+
+        $employee->update($request->all());
+
+        return response([ 'employee' => new 
+        EmployeeResource($employee), 'message' => 'Success'], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Employee  $employee
+     * @param \App\Employee $employee
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
     public function destroy(Employee $employee)
     {
-        //
+        $employee->delete();
+
+        return response(['message' => 'Employee deleted']);
     }
 }
